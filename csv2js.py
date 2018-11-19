fbbefd11657dddfe5de5d9987657668108e2aa5d
Markus Jonsson <markusrobertjonsson@gmail.com>
2018-11-19 18:13:51 +0100

    Beta.

--- csv2js.py
+++ csv2js.py
@@ -27,10 +27,57 @@ for line in lines:
     assert(key not in ybd)
     ybd[key] = day_of_year
 
+group_tropik = ['Näktergal', 'Blåhake', 'Rödstjärt', 'Buskskvätta', 'Stenskvätta', 'Gräshoppsångare', 'Sävsångare', 'Kärrsångare',
+'Rörsångare', 'Härmsångare', 'Höksångare', 'Ärtsångare', 'Törnsångare', 'Trädgårdssångare', 'Svarthätta', 'Grönsångare', 'Lövsångare',
+'Grå flugsnappare', 'Mindre flugsnappare', 'Svartvit flugsnappare', 'Törnskata', 'Rosenfink', 'Göktyta']
+group_medelkort = ['Svart rödstjärt', 'Koltrast', 'Rödvingetrast', 'Gransångare', 'Bofink', 'Bergfink', 'Gulsparv', 'Sävsparv',
+'Steglits', 'Gärdsmyg', 'Järnsparv', 'Rödhake', 'Björktrast', 'Kungsfågel', 'Blåmes', 'Talgoxe', 'Grönfink', 'Grönsiska', 'Gråsiska']
+
+tropik_average = dict()
+tropik_count = dict()
+medelkort_average = dict()
+medelkort_count = dict()
+
+for key in ybd:
+    year = key[0]
+    bird = key[1]
+    val = ybd[key]
+    if val is None:
+        continue
+    if bird in group_tropik:
+        if year in tropik_average:
+            tropik_average[year] += val
+            tropik_count[year] += 1
+        else:
+            tropik_average[year] = val
+            tropik_count[year] = 1
+    if bird in group_medelkort:
+        if year in medelkort_average:
+            medelkort_average[year] += val
+            medelkort_count[year] += 1
+        else:
+            medelkort_average[year] = val
+            medelkort_count[year] = 1
+
+# print(tropik_average)
+# print(tropik_count)
+# print(medelkort_average)
+# print(medelkort_count)
+
+for year in tropik_average:
+    tropik_average[year] /= tropik_count[year]
+    ybd[(year, 'tropik')] = tropik_average[year]
+for year in medelkort_average:
+    medelkort_average[year] /= medelkort_count[year]
+    ybd[(year, 'medelkort')] = medelkort_average[year]
+
 out_matrix = list()
 
 sorted_years = sorted(years)
 sorted_birds = sorted(birds)
+sorted_birds.append('tropik')
+sorted_birds.append('medelkort')
+
 for i, year in enumerate(sorted_years):
     out_matrix.append(list())
     for bird in sorted_birds:
@@ -46,12 +93,13 @@ column_headers = list(sorted_birds)
 column_headers.insert(0, 'Year')
 out += str(column_headers) + ',\n'
 for i, year in enumerate(sorted_years):
-    print(type(year))
     row = list(out_matrix[i])
     # row.insert(0, '"' + str(year) + '"')
     row.insert(0, "new Date(" + str(year) + ", 1, 1)")
     out += str(row).replace("'", "") + ",\n"
 out += "        ];"
+
+
 
 # var arr = [
 #           ['Year', 'Art1', 'Art2'],
